08c3a4f0e4ee87fb247c00fdf4188ffa3f1b6513
DESKTOP-E8OEJA7\schnu <markus-ws-weiss@outlook.de>
2019-02-04 16:39:10 +0100

    Set CircleObjekts

--- PatenteStereopsis/HFU_PatenteStereopsisProblem_WS1819/Assets/ObjectController_New.cs
+++ PatenteStereopsis/HFU_PatenteStereopsisProblem_WS1819/Assets/ObjectController_New.cs
@@ -7,30 +7,43 @@ public class ObjectController_New : MonoBehaviour {
 
     // Nah und FernPunkt
     // TopDownCircle
-    private GameObject fernPunktCircle;
-    [Range(4,30)]
-    public float fernPunktScale;
+    private GameObject fernObjektCircle;
+    [Range(2,30)]
+    public float fernObjektScale;
 
-    private GameObject nahPunktCircle;
-    [Range(4, 30)]
-    public float NahpunktScale;
+    private GameObject nahObjektCircle;
+    [Range(2, 30)]
+    public float nahObjektScale;
 
 
-    // VR Camera CirclePlanObjects
+    public float circleObjektScaleFaktor = 2f;
+    private GameObject circlePlaneNahPunktObjekt;
+    private GameObject circlePlaneFernPunktObjekt;
+
+
+    //CirclePlanObjects
     private GameObject circlePlaneParent;
+    private Transform playerVRCamera;
 
-    private GameObject circlePlaneNahPunkt;
-    private GameObject circlePlaneFernPunkt;
+
+
 
 
     private void Awake()
     {
-        circlePlaneParent = GameObject.Find("CircleParent");
-        circlePlaneFernPunkt = GameObject.Find("CirclePlanFernPunkt");
-        circlePlaneNahPunkt = GameObject.Find("CirclePlaneNahPunkt");
+        circlePlaneNahPunktObjekt = GameObject.Find("CirclePlaneNahObjekt");
+        circlePlaneFernPunktObjekt = GameObject.Find("CirclePlaneFernObjekt");
 
-        fernPunktCircle = GameObject.Find("FernPunktCircle");
-        nahPunktCircle = GameObject.Find("NahPunktCircle");
+        //Get User Camera
+        playerVRCamera = GameObject.Find("Camera").GetComponentInChildren<Transform>();
+
+        //Circles Around
+        fernObjektCircle = GameObject.Find("FernObjektCircle");
+        nahObjektCircle = GameObject.Find("NahObjektCircle");
+
+        circlePlaneParent = GameObject.Find("CircleParent");
+        //circlePlaneFernPunkt = GameObject.Find("CirclePlaneFernObjekt");
+        //circlePlaneNahPunkt = GameObject.Find("CirclePlaneNahObjekt");
     }
 
     // Use this for initialization
@@ -40,18 +53,43 @@ public class ObjectController_New : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        CircleScale(fernPunktCircle, fernPunktScale);
-        CircleScale(nahPunktCircle, NahpunktScale);
+        CircleScale(fernObjektCircle, fernObjektScale);
+        CircleScale(nahObjektCircle, nahObjektScale);
+        SetVRCameraOrientaionToObjectParent(playerVRCamera);
 
-	}
+        // die Position kann über den Scale gesetzt werden
+        setCircleObjectPositions(circlePlaneNahPunktObjekt, nahObjektScale);
+        setCircleObjectPositions(circlePlaneFernPunktObjekt, fernObjektScale);
+
+        //Der Scale des Objekts wird über den Faktor gesetzt
+        setCircleObjectScale(circlePlaneFernPunktObjekt, circleObjektScaleFaktor);
+        setCircleObjectScale(circlePlaneFernPunktObjekt, circleObjektScaleFaktor);
+
+    }
+
+    //Holt VR-Camera Orientation und setzt sie auf den ObjektParent
+    public void SetVRCameraOrientaionToObjectParent(Transform camera)
+    {
+        circlePlaneParent.transform.rotation = camera.rotation;
+    }
 
     public void CircleScale(GameObject objectToScale ,float scale)
     {
         objectToScale.transform.localScale = new Vector3(scale / 60, scale / 60, scale / 60);
     }
 
-    public void setCirclePositions(string valuename)
+    public void setCircleObjectPositions(GameObject circlePlaneObject, float position)
     {
+        var tp =  transform.position;
+        circlePlaneObject.transform.position = new Vector3(tp.x, tp.y, tp.z + position);
+        //print(circlePlaneObject.transform.position.z);
+    }
 
+    public void setCircleObjectScale(GameObject circlePlaneObject, float scale)
+    {
+        var ts = transform.localScale;
+        circlePlaneObject.transform.localScale = new Vector3(ts.x , ts.y , ts.z);
+        //circlePlaneObject.transform.localScale = new Vector3(tp.x * scale, tp.y * scale, tp.z * scale);
+        //print(circlePlaneObject.transform.position.z);
     }
 }
