092feb1c13fdd541f7518ff6f1d4b0d80016265d
DESKTOP-E8OEJA7\schnu <markus-ws-weiss@outlook.de>
2019-02-11 01:04:48 +0100

    Beta 1.3 / Noise Shader posible

--- Kopfweg_2Folder/Kopfweh_2/Assets/scale_Mar.cs
+++ Kopfweg_2Folder/Kopfweh_2/Assets/scale_Mar.cs
@@ -25,13 +25,14 @@ public class scale_Mar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         scaleFern = objectController.GetComponent<ObjectController_New>().fernObjektScale;
         scaleNah = objectController.GetComponent<ObjectController_New>().nahObjektScale;
         print(scaleFern - scaleNah);
 
         GetComponent<MeshRenderer>().material.SetVector("_v1", new Vector3(scaleFern * factor, scaleFern * factor, scaleFern  * factor));
         GetComponent<MeshRenderer>().material.SetVector("_v1", new Vector3((scaleFern - scaleNah) * factor - scaleFern, (scaleFern - scaleNah) * factor - scaleFern, (scaleFern - scaleNah) * factor - scaleFern));
-        
+        */
 
 /*
         if (scaleNah <= 2f)
